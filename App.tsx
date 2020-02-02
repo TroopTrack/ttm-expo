b0645609afaa4f019256f80ac6ba84b56cdb3eb4
David Christiansen <dave@trooptrack.com>
2020-02-01 20:38:38 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView
+      source={{ uri: 'https://trooptracktest.com/user_account_session/new' }}
+      style={{ marginTop: 20 }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
