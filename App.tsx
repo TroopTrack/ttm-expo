f21706bb64a473b95ee8c08146fbcadbfe31b881
David Christiansen <dave@trooptrack.com>
2020-02-01 23:30:25 -0700

    And it works

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export default function App() {
   return (
     <WebView
       source={{ uri: 'https://trooptracktest.com/user_account_session/new' }}
-      style={{ marginTop: 20 }}
+      userAgent="TroopTrackMobile"
+      style={styles.container}
     />
   );
 }
 
+const height = StatusBar.currentHeight + 5;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: getStatusBarHeight(true),
   },
 });
