27118391a4c80fb184f28e2eb42bccb23f073d0c
David Christiansen <dave@trooptrack.com>
2020-02-12 21:28:34 -0700

    Clear the old token if it's garbage

--- components/TroopTrack/index.tsx
+++ components/TroopTrack/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import Login from '../Login';
-import { styles } from '../Styles';
 import LoginStore from '../Login/store';
 import LoginReactions from '../Login/LoginReactions';
 import AppStore, { appStore } from '../../AppStore';
 import registerForPushNotificationsAsync from '../../AppStore/pushNotifications';
 import CustomHeaderWebView from './CustomHeaderWebView';
-import { just } from 'maybeasy';
 import Task from 'taskarian';
 import { successfulLoginDecoder, SuccessfulLogin } from '../../Appy';
+
+const removeData = async () => {
+  try {
+    await AsyncStorage.removeItem('@tt_token');
+  } catch (e) {
+    // saving error
+  }
+};
 
 @observer
 class TroopTrack extends React.Component {
@@ -18,16 +24,19 @@ class TroopTrack extends React.Component {
     Task.fromPromise<any, string>(() => AsyncStorage.getItem('@tt_token')).fork(
       () => console.log('No stored login'),
       loginS => {
-        // console.log(loginS);
-        successfulLoginDecoder.decodeJson(loginS).cata({
-          Err: err => {
-            console.log(err);
-            return;
-          },
-          Ok: (login: SuccessfulLogin) => {
-            appStore.loggedIn(login);
-          },
-        });
+        console.log(loginS);
+        if (loginS) {
+          successfulLoginDecoder.decodeJson(loginS).cata({
+            Err: err => {
+              console.log(err);
+              removeData();
+              return;
+            },
+            Ok: (login: SuccessfulLogin) => {
+              appStore.loggedIn(login);
+            },
+          });
+        }
       }
     );
   };
